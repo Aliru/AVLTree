3e6ef8a4f63187c84faeac98f74b9deed6ce31d2
Руслан <@192.168.1.107>
2014-05-24 19:38:57 +0300

    ArrayList

--- AVLTree/src/avl_tree/BTreeDisplay.java
+++ AVLTree/src/avl_tree/BTreeDisplay.java
@@ -3,6 +3,7 @@ package avl_tree;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -13,33 +14,40 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
-public class BTreeDisplay extends JLabel
+public class BTreeDisplay extends AVLPaint
 {
 	static JFrame jfrm;
 	static JPanel panel;
+	
+	static ArrayList <NodeEdge> Tree = new ArrayList();
+	
 	static 
 	{
 	jfrm = new JFrame("Paint Demo");
 	panel = new JPanel();
 	jfrm.add(panel); 
 	jfrm.setSize(500, 500);
-	jfrm.setLocationByPlatform(true);
+	jfrm.setLocationRelativeTo(null);
 	jfrm.setVisible(true);
 	panel.setLayout(null);
+	//panel.setBounds(0, 0, 250, 250);
 	}
 
 	BTreeDisplay(AVLNode tree, int x, int y)
 	{ 
+		
 		if (tree != null) 
 		{ 
 			String value = String.valueOf(tree.value); 
 			int pos = SwingConstants.CENTER;
-			setText(value);
-			panel.add(this);
+			//Tree.add(new NodeEdge(250, 50, ));
 			setBounds(new Rectangle(new Point(x, y), getPreferredSize()));
-			panel.add(new BTreeDisplay(tree.left, x - 20, y + 20));
-			panel.add(new BTreeDisplay(tree.right, x + 20, y + 20)); 
+			panel.add(new BTreeDisplay(tree.left, x - 25, y + 20));
+			panel.add(new BTreeDisplay(tree.right, x + 15, y + 20)); 
 		} 
 	}
 }		
--- AVLTree/src/avl_tree/Test.java
+++ AVLTree/src/avl_tree/Test.java
@@ -1,20 +1,26 @@
 package avl_tree;
 
 import javax.swing.SwingUtilities;
+import javax.swing.JOptionPane;
 
 public class Test {
 
 	public static void main(String[] args)
 	{
 		  AVLTree tree = new AVLTree(10,5,20,8,128,88,4,7,2);
-		  //AVLTree tree = new AVLTree(10,5,20);
 		  final AVLNode root = tree.getRoot();
-		 //AVLNode root = tree.getRoot();
+		  //AVLNode root = tree.getRoot();
 		  System.out.println(root.getValue() + ", with height " + root.getHeight());
 		  System.out.println(root.getLeft().getValue() + ", with height " + root.getLeft().getHeight());
 		  System.out.println(root.getRight().getValue() + ", with height " + root.getRight().getHeight());
 		  tree.print();
-		  tree.delete(8);
+		  //String key = JOptionPane.showInputDialog( "Enter key" ); //add Exceptions
+		  //int value = Integer.parseInt(key); 
+		  //tree.delete(value);
+		  SwingUtilities.invokeLater(new Runnable() { public void run() {new BTreeDisplay(root, 250, 50);}});
+		  String key = JOptionPane.showInputDialog( "Enter key" ); //add Exceptions
+		  int value = Integer.parseInt(key); 
+		  tree.delete(value);
 		  SwingUtilities.invokeLater(new Runnable() { public void run() {new BTreeDisplay(root, 250, 50);}});
 		  //System.out.println(root.getLeft().getRight().getValue() + ", with height " + root.getLeft().getRight().getLeft().getHeight());
 		  //System.out.println(root.getRight().getRight().getValue() + ", with height " + root.getRight().getRight().getHeight());
