d8ee4fe1c3625bf580901345fbc144278663b464
Руслан <@192.168.1.107>
2014-05-25 23:42:38 +0300

    1

--- AVLTree/src/avl_tree/Test.java
+++ AVLTree/src/avl_tree/Test.java
@@ -7,7 +7,8 @@ public class Test {
 
 	public static void main(String[] args) throws Exception
 	{
-		  AVLTree tree = new AVLTree(10,5,20,8,128,88,4,7,2);
+		  //AVLTree tree = new AVLTree(10,5,20,8,128,88,4,7,2);
+		  AVLTree tree = new AVLTree(10,5,20,8);
 		  final AVLNode root = tree.getRoot();
 		  //AVLNode root = tree.getRoot();
 		  System.out.println(root.getValue() + ", with height " + root.getHeight());
@@ -18,18 +19,14 @@ public class Test {
 		  //int value = Integer.parseInt(key); 
 		  //tree.delete(value);
 		  tree.insert(75);
-		  tree.insert(0);
+		  //tree.insert(0);
 		  System.out.println(tree.getRoot().value);
 		  tree.print();
-		  SwingUtilities.invokeLater(new Runnable() 
-		  { 
-			  public void run() 
-			  {
-			  BTreeDisplay.Tree.add(new BTreeDisplay(root, 250, 50)); 
-			  BTreeDisplay.panel.invalidate(); 
-			  BTreeDisplay.panel.repaint();
-			  }
-		  });
+		  updateScreenTree(tree.getRoot());
+		  Thread.sleep(10000);
+		  tree.insert(36);
+		  updateScreenTree(tree.getRoot());
+		  //
 		  //String key = JOptionPane.showInputDialog( "Enter key" ); //add Exceptions
 		  //int value = Integer.parseInt(key); 
 		  //tree.delete(value);
@@ -38,5 +35,22 @@ public class Test {
 		  //System.out.println(root.getRight().getRight().getValue() + ", with height " + root.getRight().getRight().getHeight());
 		  //tree.print();  //new tree print for check GitHub
 	}
+	
+	static public void updateScreenTree(final AVLNode Root)
+	  {
+		  SwingUtilities.invokeLater(new Runnable() 
+		  { 
+		  public void run() 
+		  {  
+			  if (!BTreeDisplay.Tree.isEmpty())
+				  BTreeDisplay.Tree.clear();
+			  {
+				  BTreeDisplay.Tree.add(new BTreeDisplay(Root, 250, 50)); 
+				  BTreeDisplay.panel.invalidate(); 
+				  BTreeDisplay.panel.repaint();
+			  }
+		  }
+		  });
+	  }
 
 }
